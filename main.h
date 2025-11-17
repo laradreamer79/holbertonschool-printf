5d59a6d9292393bed6e0ef2385b3482d49853258
laradreamer79 <laradreamer79@gmail.com>
2025-11-17 10:37:39 +0000

    main.h

---
+++ main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+#include <unistd.h>
+
+/* Function prototypes */
+int _printf(const char *format, ...);
+int _putchar(char c);
+int print_string(char *str);
+int print_number(int n);
+
+#endif /* MAIN_H */
+
