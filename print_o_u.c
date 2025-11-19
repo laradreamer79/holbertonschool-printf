8c8d63844b26fed99f9e10028652466c92dde7d8
haitham71 <hri7isd@gmail.com>
2025-11-19 14:46:10 +0300

    Add the new functions for octal and unsigned

---
+++ print_o_u.c
@@ -0,0 +1,68 @@
+#include "main.h"
+
+/**
+ * print_octal_rec - recursive helper to print octal number
+ * @n: number to print
+ *
+ * Return: number of characters printed
+ */
+int print_octal_rec(unsigned int n)
+{
+	int count = 0;
+
+	if (n / 8)
+		count += print_octal_rec(n / 8);
+
+	count += _putchar((n % 8) + '0');
+	return (count);
+}
+
+/**
+ * print_octal - prints an unsigned int in octal format
+ * @args: argument list
+ *
+ * Return: number of characters printed
+ */
+int print_octal(va_list args)
+{
+	unsigned int n = va_arg(args, unsigned int);
+
+	if (n == 0)
+		return (_putchar('0'));
+
+	return (print_octal_rec(n));
+}
+
+
+/**
+ * print_unsigned_rec - recursive helper to print unsigned integer
+ * @n: number to print
+ *
+ * Return: number of characters printed
+ */
+int print_unsigned_rec(unsigned int n)
+{
+	int count = 0;
+
+	if (n / 10)
+		count += print_unsigned_rec(n / 10);
+
+	count += _putchar((n % 10) + '0');
+	return (count);
+}
+
+/**
+ * print_unsigned - prints an unsigned integer
+ * @args: argument list
+ *
+ * Return: number of characters printed
+ */
+int print_unsigned(va_list args)
+{
+	unsigned int n = va_arg(args, unsigned int);
+
+	if (n == 0)
+		return (_putchar('0'));
+
+	return (print_unsigned_rec(n));
+}
