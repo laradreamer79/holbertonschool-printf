6b380e8a7a226b7f267eb9f2643e9ec5eca7038a
haitham71 <hri7isd@gmail.com>
2025-11-19 14:36:52 +0300

    Add ability to convert numbers to hex, binary, octal, and unsigned

--- _printf.c
+++ _printf.c
@@ -1,3 +1,4 @@
+
 #include "main.h"
 
 /**
@@ -8,40 +9,45 @@
  */
 int _printf(const char *format, ...)
 {
-    va_list args;
-    int i = 0;
+va_list args;
+int i = 0;
+if (!format || (format[0] == '%' && format[1] == '\0'))
+return (-1);
+va_start(args, format);
+while (*format)
+{
+if (*format == '%')
+{
+format++;
+if (*format == 'c')
+    i += _putchar(va_arg(args, int));
+else if (*format == 's')
+    i += print_string(va_arg(args, char *));
+else if (*format == '%')
+    i += _putchar('%');
+else if (*format == 'd' || *format == 'i')
+    i += print_number(va_arg(args, int));
+else if (*format == 'b')
+    i += print_binary(args);
+else if (*format == 'u')
+    i += print_unsigned(args);
+else if (*format == 'o')
+    i += print_octal(args);
+else if (*format == 'x')
+    i += print_hex_lower(args);
+else if (*format == 'X')
+    i += print_hex_upper(args);
+else
+{
+    i += _putchar('%');
+    i += _putchar(*format);
+}
 
-    if (!format || (format[0] == '%' && format[1] == '\0'))
-        return (-1);
-
-    va_start(args, format);
-
-    while (*format)
-    {
-        if (*format == '%')
-        {
-            format++;
-            if (*format == 'c')
-                i += _putchar(va_arg(args, int));
-            else if (*format == 's')
-                i += print_string(va_arg(args, char *));
-            else if (*format == '%')
-                i += _putchar('%');
-            else if (*format == 'd' || *format == 'i')
-                i += print_number(va_arg(args, int));
-            else
-            {
-                i += _putchar('%');
-                i += _putchar(*format);
-            }
-        }
-        else
-        {
-            i += _putchar(*format);
-        }
-        format++;
-    }
-
-    va_end(args);
-    return (i); 
+}
+else
+i += _putchar(*format);
+format++;
+}
+va_end(args);
+return (i);
 }
