62ee2c978afaf6a3653c64d01f600c845bda0ec5
haitham71 <hri7isd@gmail.com>
2025-11-20 19:12:19 +0300

    dd support for custom specifiers: 'r' for reversed string and 'p' for pointers and remove unused files

---
+++ print_p.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_pointer - prints a pointer address in hexadecimal (0x...)
+ * @args: argument list
+ *
+ * Return: number of characters printed
+ */
+int print_pointer(va_list args)
+{
+    unsigned long long ptr = (unsigned long long)va_arg(args, void *);
+    char hex[20];
+    char *symbols = "0123456789abcdef";
+    int i = 0, len = 0;
+
+    if (ptr == 0)
+        return (print_string("(nil)"));
+
+    while (ptr)
+    {
+        hex[len++] = symbols[ptr % 16];
+        ptr /= 16;
+    }
+
+    i += _putchar('0');
+    i += _putchar('x');
+
+    while (len--)
+        i += _putchar(hex[len]);
+
+    return (i);
+}
